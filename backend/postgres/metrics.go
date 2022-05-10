893ebc25e4ac0b4cee22e980f90cd0fad1effdc8
Claude Mokbel <claudemokbel@gmail.com>
2022-05-09 17:40:40 -0700

    remove user_app_credentials metrics gauge
    removed the user app credentials metrics gauge because the table no
    longer exists, this was generating a lot of unnecessary output.

--- backend/postgres/metrics.go
+++ backend/postgres/metrics.go
@@ -10,19 +10,17 @@ import (
 )
 
 var metrics = struct {
-	userCountGauge              prometheus.Gauge
-	authCountGauge              prometheus.Gauge
-	userAppCredentialCountGauge prometheus.Gauge
-	sessionCountGauge           prometheus.Gauge
-	todoItemCountGauge          prometheus.Gauge
-	todoListCountGauge          prometheus.Gauge
+	userCountGauge     prometheus.Gauge
+	authCountGauge     prometheus.Gauge
+	sessionCountGauge  prometheus.Gauge
+	todoItemCountGauge prometheus.Gauge
+	todoListCountGauge prometheus.Gauge
 }{
-	userCountGauge:              promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_users", Help: "Total number of users"}),
-	authCountGauge:              promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_auths", Help: "Total number of auths"}),
-	userAppCredentialCountGauge: promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_user_app_credentials", Help: "Total number of user app credentials"}),
-	sessionCountGauge:           promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_sessions", Help: "Total number of active sessions"}),
-	todoItemCountGauge:          promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_todo_items", Help: "Total number of todo items"}),
-	todoListCountGauge:          promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_todo_lists", Help: "Total number of todo lists"}),
+	userCountGauge:     promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_users", Help: "Total number of users"}),
+	authCountGauge:     promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_auths", Help: "Total number of auths"}),
+	sessionCountGauge:  promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_sessions", Help: "Total number of active sessions"}),
+	todoItemCountGauge: promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_todo_items", Help: "Total number of todo items"}),
+	todoListCountGauge: promauto.NewGauge(prometheus.GaugeOpts{Name: "todo_db_todo_lists", Help: "Total number of todo lists"}),
 }
 
 func (db *DB) monitorMetrics() {
@@ -59,11 +57,6 @@ func (db *DB) updateStats(ctx context.Context) error {
 		return fmt.Errorf("auths count: %v", err)
 	}
 	metrics.authCountGauge.Set(float64(n))
-
-	if err = tx.QueryRowContext(ctx, `SELECT COUNT(*) FROM user_app_credentials`).Scan(&n); err != nil {
-		return fmt.Errorf("user_app_credentials count: %v", err)
-	}
-	metrics.userAppCredentialCountGauge.Set(float64(n))
 
 	if err = tx.QueryRowContext(ctx, `SELECT COUNT(*) FROM sessions`).Scan(&n); err != nil {
 		return fmt.Errorf("sessions count: %v", err)
