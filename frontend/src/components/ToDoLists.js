7ea2ec8bfe7202dbc82418e08c741799d558fb1d
Claude Mokbel <claudemokbel@gmail.com>
2022-05-25 13:44:17 -0700

    commented handeListClick

--- frontend/src/components/ToDoLists.js
+++ frontend/src/components/ToDoLists.js
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { getList } from '../http/lists'
 
 export const ToDoLists = ({lists, listState, setListState}) => {
-
+// when the list button is clicked the API will return a list item and
+// we want to set that list item to a state which will then be passed up
+// this will allow us to render the current list item onto the main page
     const handleListClick = async(id) => {
         const list = await getList(id);
         if (list.ok) {
@@ -11,7 +13,7 @@ export const ToDoLists = ({lists, listState, setListState}) => {
             return list.error
         }
     }
-    
+
     return (
         <div>
             <ul className="list-group">
