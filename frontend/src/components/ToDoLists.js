f2c45a0f0f4d84147edc05bffbc8d482b8661e28
Claude Mokbel <claudemokbel@gmail.com>
2022-05-25 08:30:29 -0700

    added placeholder list

--- frontend/src/components/ToDoLists.js
+++ frontend/src/components/ToDoLists.js
@@ -3,8 +3,13 @@ import { useState } from 'react';
 export const ToDoLists = ({userState}) => {
     return (
         <div>
-            <ul>
-                <li><a href="!#">Placeholder</a></li>
+            <ul className="list-group">
+                <li className="list-group-item active"><a className="link-dark" href="!#">Placeholder</a></li>
+                <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
+                <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
+                <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
+                <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
+
                 {/* user.lists.map(list => <li><button>list.name</button></li>) */}
             </ul>
         </div>
