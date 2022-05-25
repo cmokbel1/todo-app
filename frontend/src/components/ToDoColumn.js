05b2a7c75371c239cbd0d44713a6ebee6db8f71b
Claude Mokbel <claudemokbel@gmail.com>
2022-05-25 08:44:08 -0700

    removed a ul duplicate  from ToDoColumn

--- frontend/src/components/ToDoColumn.js
+++ frontend/src/components/ToDoColumn.js
@@ -10,9 +10,7 @@ const ToDoColumn = ({userState}) => {
     return (
         <div className="col-sm-3" style={{ border: '1px solid red' }}>
             <p className="fs-3 text-center">TODO</p>
-            <ul>
             <ToDoLists userState={userState}/>
-            </ul>
             <div className="text-center">
             <a className="link-dark" target="_blank" rel="noreferrer" href="https://github.com/cmokbel1/todo-app">{gitHub}</a>
             </div>
--- frontend/src/components/ToDoLists.js
+++ frontend/src/components/ToDoLists.js
@@ -9,7 +9,6 @@ export const ToDoLists = ({userState}) => {
                 <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
                 <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
                 <li className="list-group-item"><a className="link-dark" href="!#">Placeholder</a></li>
-
                 {/* user.lists.map(list => <li><button>list.name</button></li>) */}
             </ul>
         </div>
