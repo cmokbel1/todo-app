f8cfe168f6a4e2e21d84c821d1baa7e4e6a0c186
Claude Mokbel <claudemokbel@gmail.com>
2022-06-03 12:14:29 -0700

    bootstrap modification to login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
     <>
       <Header userState={userState} setUserState={setUserState} setReturnError={setReturnError} />
       <div className="container-fluid">
-        <div className="row">
+        <div className="row justify-content-center" style={{ width: '100%' }}>
           <Main userState={userState} setUserState={setUserState} setReturnError={setReturnError} />
         </div>
         <Footer />
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -13,27 +13,30 @@ const Login = ({ setUserState }) => {
         event.preventDefault();
         loginUser(userTrim, passTrim).then(res => {
             if (res.name) {
-                 setUserState(res.name);
+                setUserState(res.name);
             } else {
                 setErrorState(res);
             }
         });
     }
     return (
-        <div className="offset-3 col-6">
-            <form className="card justify-content-center" style={{ width: '18em' }}>
-                <p className="card-title">You must login to continue</p>
-                <div className="input col-sm-8 mb-3">
-                    <label className="input-text" htmlFor="username">username</label>
-                    <input type="text" id="username" name="username" className="form-control" aria-label="username-input" aria-describedby="username-input" onChange={(e) => setUserInput(e.target.value)} />
+        <div className="col-6">
+            <form className="card" style={{ width: '18em' }}>
+                <div className="card-body">
+                    <p className="card-title">You must login to continue</p>
+                    <div className="input mb-3">
+                        <label className="input-text" htmlFor="username">username</label>
+                        <input type="text" id="username" name="username" className="form-control" aria-label="username-input" aria-describedby="username-input" onChange={(e) => setUserInput(e.target.value)} />
+                    </div>
+                    <div className="input mb-3">
+                        <label className="input-text" htmlFor="password">password</label>
+                        <input type="password" id="password" name="password" className="form-control" aria-label="password-input" aria-describedby="password-input" onChange={(e) => setPasswordInput(e.target.value)} />
+                    </div>
+                    <p style={{ color: 'red' }}>{errorState}</p>
+                    <div className="text-center">
+                        <button type="submit" className="btn btn-primary w-75 text-center" onClick={handleLogin} disabled={userInput === "" || passwordInput === ""}>Login</button>
+                    </div>
                 </div>
-                <div className="input col-sm-8 mb-3">
-                    <label className="input-text" htmlFor="password">password</label>
-                    <input type="password" id="password" name="password" className="form-control" aria-label="password-input" aria-describedby="password-input" onChange={(e) => setPasswordInput(e.target.value)} />
-                </div>
-                <p style={{color: 'red'}}>{errorState}</p>
-                <button type="submit" className="btn btn-primary" onClick={handleLogin} disabled={userInput === "" || passwordInput === ""}>Login</button>
-                
             </form>
         </div>
     )
