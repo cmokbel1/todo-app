f12805e1310eb6bad5eccea882a2b9b4c61bec88
Claude Mokbel <claudemokbel@gmail.com>
2022-06-03 09:02:52 -0700

    changed width of inputs

--- frontend/src/components/ListDetail.js
+++ frontend/src/components/ListDetail.js
@@ -70,7 +70,7 @@ export const ListDetail = ({ id, name, completed, items, handleUpdate, removeLis
     let body = <h1>Nothing to see here</h1>
     if (id) {
         body = <div className="container text-center col-10 border border-dark rounded">
-            <input className="fs-3 mb-4 mt-2 text-center" rows="2" type="text" value={currentName} onChange={(e) => setCurrentName(e.target.value)} onKeyPress={(e) => handleListUpdate(e)}></input>
+            <input className="fs-3 mb-4 mt-2 text-center w-50" rows="2" type="text" value={currentName} onChange={(e) => setCurrentName(e.target.value)} onKeyPress={(e) => handleListUpdate(e)}></input>
             <ul className="list-group mb-4">
                 {itemsState.map((item, index) => {
                     return <Item id={item.id} name={item.name}
@@ -79,7 +79,7 @@ export const ListDetail = ({ id, name, completed, items, handleUpdate, removeLis
                         key={index} deleteItem={handleDeleteItem} />
                 })}
             </ul>
-            <input type="text" name="item" className="form-input w-25"
+            <input type="text" name="item" className="form-input w-50"
                 onChange={(e) => { setNewItemName(e.target.value) }} onKeyPress={(e) => handleAddItem(e)}
                 placeholder="Add Item" value={newItemName}></input>
             <p className="text-center">{messageState}</p><p className="text-center" style={{ color: 'red' }}>{errorMessageState}</p>
