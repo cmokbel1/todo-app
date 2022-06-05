0898b131db6f04805691089abf0946e590785c18
Claude Mokbel <claudemokbel@gmail.com>
2022-06-05 08:29:22 -0700

    addressed PR feedback

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -20,22 +20,20 @@ const Login = ({ setUserState }) => {
         });
     }
     return (
-        <div className="col-6">
-            <form className="card" style={{ width: '18em' }}>
+        <div className="col-12 offset-md-4 col-md-4">
+            <form className="card mx-auto px-4 py-2">
                 <div className="card-body">
-                    <p className="card-title">You must login to continue</p>
+                    <p className="card-title lead text-center mb-4">You must login to continue</p>
                     <div className="input mb-3">
-                        <label className="input-text" htmlFor="username">username</label>
+                        <label className="input-text pb-2" htmlFor="username">Username</label>
                         <input type="text" id="username" name="username" className="form-control" aria-label="username-input" aria-describedby="username-input" onChange={(e) => setUserInput(e.target.value)} />
                     </div>
                     <div className="input mb-3">
-                        <label className="input-text" htmlFor="password">password</label>
+                        <label className="input-text pb-2" htmlFor="password">Password</label>
                         <input type="password" id="password" name="password" className="form-control" aria-label="password-input" aria-describedby="password-input" onChange={(e) => setPasswordInput(e.target.value)} />
                     </div>
                     <p style={{ color: 'red' }}>{errorState}</p>
-                    <div className="text-center">
-                        <button type="submit" className="btn btn-primary w-75 text-center" onClick={handleLogin} disabled={userInput === "" || passwordInput === ""}>Login</button>
-                    </div>
+                    <button type="submit" className="btn btn-primary w-100 text-center mt-3" onClick={handleLogin} disabled={userInput === "" || passwordInput === ""}>Login</button>
                 </div>
             </form>
         </div>
