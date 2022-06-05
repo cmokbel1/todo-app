a6d2ade7133839616493c8fe991590319b1561eb
Claude Mokbel <claudemokbel@gmail.com>
2022-06-05 09:33:38 -0700

    created flash message component boiler plate

---
+++ frontend/src/components/FlashMessage.js
@@ -0,0 +1,22 @@
+
+
+export const FlashMessage = ({ errorMessage, successMessage }) => {
+
+    if (errorMessage) {
+        return (
+            <div className="container" style={{backgroundColor: 'red'}}>
+                <div className="text-center">
+                    <p>{errorMessage}</p>
+                </div>
+            </div>
+        )
+    } else if (successMessage) {
+        return (
+            <div className="container" style={{backgroundColor: 'green'}}>
+                <div className="text-center">
+                    <p>{successMessage}</p>
+                </div>
+            </div>
+        )
+    }
+}
