e863449c1157c7ffe6b1a2068a406083acbe2c1a
George Mokbel <georgemokbel@gmail.com>
2022-06-09 06:53:01 -0400

    Add API wide rate limit, default timeouts for HTTP server and global user create rate limit

--- backend/http/server.go
+++ backend/http/server.go
@@ -13,6 +13,7 @@ import (
 	"github.com/cmokbel1/todo-app/backend/todo"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/httprate"
 )
 
 type Server struct {
@@ -36,7 +37,11 @@ type Server struct {
 
 func NewServer() *Server {
 	s := &Server{
-		server:           &http.Server{},
+		server: &http.Server{
+			ReadTimeout:  time.Second * 6,
+			WriteTimeout: time.Second * 6,
+			IdleTimeout:  time.Second * 6,
+		},
 		Logger:           todo.NewLogger(),
 		LoggerMiddleware: middleware.Logger,
 	}
@@ -54,6 +59,8 @@ func (s *Server) Listen() (err error) {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
+	// hard coded rate limit of 60 requests/minute/IP
+	r.Use(httprate.LimitByIP(60, time.Minute))
 	r.Use(s.LoggerMiddleware)
 	r.Use(s.cors)
 	r.Use(s.sessionMiddleware)
