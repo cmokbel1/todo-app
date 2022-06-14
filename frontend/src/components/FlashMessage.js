d8982c894cf01e8e644d631fcd33f759bc1236ff
Claude Mokbel <claudemokbel@gmail.com>
2022-06-14 07:44:09 -0700

    flash message appears over the application instead of shifting down

--- frontend/src/components/FlashMessage.js
+++ frontend/src/components/FlashMessage.js
@@ -1,7 +1,7 @@
 export const FlashMessage = ({ messageState, returnError }) => {
     if (messageState) {
         return (
-            <div className="container mb-4 alert alert-success">
+            <div className="container alert alert-success flash w-75">
                 <div className="text-center">
                     <p>{messageState}</p>
                 </div>
@@ -9,7 +9,7 @@ export const FlashMessage = ({ messageState, returnError }) => {
         )
     } else if (returnError) {
         return (
-            <div className="container mb-4 alert alert-danger">
+            <div className="container alert alert-danger flash w-75">
                 <div className="text-center">
                     <p>{returnError}</p>
                 </div>
