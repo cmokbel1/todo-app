9ac83f72711ff9a3fe2898613eb43f5905871a24
Claude M <68614831+cmokbel1@users.noreply.github.com>
2022-06-16 07:47:06 -0700

    Bugs/click list (#32)
    * trying to fix list click bug
    
    * fixed overlap issue by rendering classes to component post state change
    
    * removed !important, not necessary
    
    * removed extra space

--- frontend/src/components/FlashMessage.js
+++ frontend/src/components/FlashMessage.js
@@ -1,15 +1,16 @@
 export const FlashMessage = ({ messageState, setMessageState, returnError }) => {
     let text;
-    let classes = "flash container text-center alert ";
+    let classes = "flash ";
+    let similarClasses = "container text-center alert"
 
     if (messageState) {
-        classes += "alert-success";
+        classes += `${similarClasses} alert-success`;
         text = messageState;
         setTimeout(() => {
             setMessageState("");
         }, 1000)
     } else if (returnError) {
-        classes += "alert-danger";
+        classes += `${similarClasses} alert-danger`;
         text = returnError;
     }
     return (
